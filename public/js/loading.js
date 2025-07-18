abe22870613a0e0d2ec311a5e138ba5b7f2a9bee
Daniel <dzirkiev95@gmail.com>
2025-07-18 08:55:48 +0300

    add About section

--- public/js/loading.js
+++ public/js/loading.js
@@ -3,5 +3,5 @@ window.addEventListener('load', function () {
     // Hide the loading screen
     document.getElementById('loading-screen').style.display = 'none';
     // Show the content
-    document.getElementById('content').style.display = 'block';
+    //document.getElementById('content').style.display = 'block';
 });
